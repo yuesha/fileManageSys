d126794f9bcc1b59a8057c0afba8cb1766015c86
yuesha <2661103378@qq.com>
2019-10-19 11:54:49 +0800

    表单验证

---
+++ behind/js/index.js
@@ -0,0 +1,22 @@
+// 检查表单数据
+function check() {
+	// 获取表单的值
+	var username = document.getElementsByName("username")[0].value;
+	var pwd = document.getElementsByName("pwd")[0].value;
+
+	// 表单项进行去除空格的处理
+	username = username.replace(/\s*/g,"");
+	pwd = pwd.replace(/\s*/g,"");
+
+	// 表单验证
+	if (username == '') {
+		alert("用户名为空，请重新输入");
+		return;
+	}else if (pwd == '') {
+		alert("密码为空，请重新输入");
+		return;
+	}else{
+		// 表单验证完成
+		alert("验证完成");
+	}
+}
